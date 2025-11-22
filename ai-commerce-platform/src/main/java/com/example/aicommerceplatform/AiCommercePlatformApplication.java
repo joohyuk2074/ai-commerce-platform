4808c848a6e0c11ee3564b938039e8338439f156
joohyuk2074 <vfrvfr4207@gmail.com>
2025-11-22 21:46:57 +0900

    Initial Commit

---
+++ ai-commerce-platform/src/main/java/com/example/aicommerceplatform/AiCommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.aicommerceplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCommercePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCommercePlatformApplication.class, args);
+    }
+
+}
---
+++ ai-commerce-platform/src/test/java/com/example/aicommerceplatform/AiCommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aicommerceplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCommercePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
